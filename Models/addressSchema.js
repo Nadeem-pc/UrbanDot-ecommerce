b8d0732016eb16322f9fd33b65e955ba4761cf81
Nadeem-pc <nadeempc777@gmail.com>
2024-12-04 09:24:08 +0530

    Small changes

--- Models/addressSchema.js
+++ Models/addressSchema.js
@@ -3,36 +3,54 @@ const User = require('../Models/userSchema')
 const ObjectId = mongoose.Schema.Types.ObjectId
 
 const addressSchema = mongoose.Schema({
+
     user : {
         type : ObjectId,
         ref : "User",
         required : true 
     },
 
-    city : {
-        type : String,
-        required : true
-    },
+    address : [{
+        addressType : {
+            type : String,
+            required : true
+        },
 
-    pincode : {
-        type : String,
-        required : true
-    },
+        name : {
+            type : String,
+            required : true
+        },
 
-    state : {
-        type : String,
-        required : true
-    },
+        phone : {
+            type : String,
+            required : true
+        },
 
-    country : {
-        type : String,
-        required : true
-    },
+        city : {
+            type : String,
+            required : true
+        },
 
-    address : {
-        type : String,
-        required : true
-    },
+        pincode : {
+            type : String,
+            required : true
+        },
+
+        state : {
+            type : String,
+            required : true
+        },
+
+        country : {
+            type : String,
+            required : true
+        },
+
+        fullAddress : {
+            type : String,
+            required : true
+        }
+    }]
 
 },{ timestamps : true })
 
