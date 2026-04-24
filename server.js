1e5765f63587d92f69b63262c32f73940ce1fe7c
Nadeem-pc <nadeempc777@gmail.com>
2026-04-24 18:53:30 +0530

    feat(server): expose currentPath to views for active navbar highlighting

--- server.js
+++ server.js
@@ -39,6 +39,7 @@ app.use(express.static(path.join(__dirname,'Public')))
 // Middleware to set session data for use in views
 app.use((req, res, next) => {
     res.locals.isLoggedIn = !!req.session.user; 
+    res.locals.currentPath = req.path;
     next();
 });
 
